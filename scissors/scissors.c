adb10f7246c6d380a1f3827bcc5fc39bcab4adf9
Vadym Kharchuk <vadym.kharchuk@globallogic.com>
2021-02-18 01:24:32 +0200

    scissors: Add functionality to parse and verify command line args
    Signed-off-by: Vadym Kharchuk <vadym.kharchuk@globallogic.com>

--- scissors/scissors.c
+++ scissors/scissors.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+enum errors {
+	NO_ARGS = 1,
+	TO_MUCH_ARGS,	/* 2 */
+	WRONG_ARG,		/* 3 */
+};
+
+int main(int argc, char const *argv[]) {
+	int ret = 0;
+
+	if (argc < 2) {
+		printf("Need one more argument\n");
+		ret = NO_ARGS;
+		goto end;
+	}
+
+	if (argc > 2) {
+		printf("To match arguments\n");
+		ret = TO_MUCH_ARGS;
+		goto end;
+	}
+
+	char user_choise[2];
+
+	if ((strcmp(argv[1], "r") == 0) ||
+		(strcmp(argv[1], "p") == 0) ||
+		(strcmp(argv[1], "s") == 0)) {
+
+		strcpy(user_choise, argv[1]);
+	} else {
+		printf("Argument can be only 'r' 'p' 's'\n");
+		ret = WRONG_ARG;
+		goto end;
+	}
+
 	printf("Make test\n");
-	return 0;
+end:
+	return ret;
 }
