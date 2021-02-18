b86322c591e49d5776dafd3e3018eb8fe8f5a666
Oleh Yakymenko <o.yakymenko22@gmail.com>
2021-02-19 00:21:24 +0200

    scissors: add game mechanics and human asking
    Add interractivity with human for possibility
    to play a game and make choice. And add game
    mechanics with rules message
    
    Signed-off-by: Oleh Yakymenko <o.yakymenko22@gmail.com>

--- scissors/main.c
+++ scissors/main.c
@@ -5,6 +5,15 @@
 int main()
 {
 	char pc_choice = gen_pc_choice();
-   	printf("%c\n", pc_choice);
+   	char hum_choice = 0;
+   	
+   	printf(">Please choose: rock (r) - paper (p) - scissors (s).  Exit - (q)\n");
+   		
+   	do {
+		printf(">");
+		hum_choice = getchar();
+		if(hum_choice == 'q') return 0;
+	}while(hum_choice != 'r' && hum_choice != 'p' && hum_choice != 's');
+	
 	return 0;
 }
