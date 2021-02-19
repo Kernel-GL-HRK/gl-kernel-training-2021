7d257618ef9daaf41898a112db65dde51a015a29
Leonid Chebenko <leonid.chebenko@globallogic.com>
2021-02-19 02:58:42 +0200

    game: Add logic

---
+++ scissors/game_logic.h
@@ -0,0 +1,21 @@
+/**
+ * @file       game_logic.h
+ * @brief      game_logic
+ * 
+ * @author     Leonid Chebenko
+ * @version    1.0
+ */
+
+#ifndef __GAME_LOGIC_H__
+#define __GAME_LOGIC_H__
+
+#define USER_WIN        1
+#define COMPUTER_WIN    2
+#define DRAW            0
+#define ROCK            1
+#define SCISSOR         2
+#define PAPER           3
+
+int game_logic(int player, int computer);
+
+#endif /* __GAME_LOGIC_H__ */
