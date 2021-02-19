bf39a2b2ba9b5fec8019c511258380ec91a26754
olyuboch <lyubochko.leha@gmail.com>
2021-02-19 11:29:53 +0200

    hwdetect: changes according code review
    Checking file for using, create files in tmp dir
    
    Signed-off-by: olyuboch <lyubochko.leha@gmail.com>

--- hwdetect/hwdetect.sh
+++ hwdetect/hwdetect.sh
@@ -2,6 +2,7 @@
 
 file_orig=file.txt
 file_cmp=new_file.txt
+path=/tmp
 var_remove=\<
 var_add=\>
 device_name_max_len=32
@@ -11,8 +12,8 @@ delay_sec=2
 function process_device {
 	local i flag=$1 string=$2
 	
-	devices=( $( diff $file_orig $file_cmp | grep $flag | \
-		cut -c 3-$device_name_max_len ) )
+	devices=( $( diff $path/$file_orig $path/$file_cmp | \
+		grep $flag | cut -c 3-$device_name_max_len ) )
 	if [ ! -z "${devices[0]}" ]
 	then
 		echo $string
@@ -23,20 +24,36 @@ function process_device {
 	fi
 }
 
+function file_checking {
+	local str=$1
+	
+	while [ -f "$path/$str" ] && [ -n "$(lsof "$path/$str")" ]
+	do
+		str=_$str
+	done
+
+	echo $str
+}
+
 echo "Press [CTRL+C] to stop.."
-ls -1 $device_dir > $file_orig
+
+# add "_" symbol to the beggining of a file name if file in use
+file_orig=$(file_checking $file_orig)
+file_cmp=$(file_checking $file_cmp)
+
+ls -1 $device_dir > $path/$file_orig
 
 while true
 do
-	ls -1 $device_dir > $file_cmp
+	ls -1 $device_dir > $path/$file_cmp
 
-	diff $file_orig $file_cmp > /dev/null
+	diff $path/$file_orig $path/$file_cmp > /dev/null
 	if [ $? -ne 0 ]
 	then
 		process_device $var_remove "Removed devices:"
 		process_device $var_add "Added devices:"
 
-		mv $file_cmp $file_orig
+		mv $path/$file_cmp $path/$file_orig
 	fi
 
 	sleep $delay_sec
