9e6095209d9714122f31d0ad6f5d19459fe9f79e
Vadym Kharchuk <vadym.kharchuk@globallogic.com>
2021-02-19 17:45:44 +0200

    scissors: refactor codebase.
    - Convert user_choice, pc_choice variables (string to char) in order to optimize codebase
    - The macro END_GAME_MESSAGE is ported to a function
    
    Signed-off-by: Vadym Kharchuk <vadym.kharchuk@globallogic.com>

--- scissors/scissors.c
+++ scissors/scissors.c
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define END_GAME_MESSAGE(user, pc, msg)	\
-	do { printf("Your choise - %s. Computer choise - %s. %s\n",	\
-			user, pc, msg);	\
-			} while(0)
+void end_game_message(char usr_val, char pc_val, char *message) 
+{
+	printf("Your choise - %c. Computer choise - %c. %s\n", usr_val, pc_val, message);
+}
 
 enum errors {
 	NO_ARGS = 1,
@@ -29,31 +29,25 @@ int main(int argc, char const *argv[]) {
 		goto end;
 	}
 
-	char user_choice[2];
-
-	if ((strcmp(argv[1], "r") == 0) ||
-		(strcmp(argv[1], "p") == 0) ||
-		(strcmp(argv[1], "s") == 0)) {
-
-		strcpy(user_choice, argv[1]);
-	} else {
+	char user_choice = *argv[1];
+	if (!(user_choice == 'r' || user_choice == 'p' || user_choice == 's')) {
 		fprintf(stderr, "Argument can be only 'r' 'p' 's'\n");
 		ret = WRONG_ARG;
 		goto end;
 	}
 
-	char game_elements[3][2] = {"r", "p", "s"};
+	const char game_elements[3] = {'r', 'p', 's'};
 	srand(time(NULL));
-	unsigned short pc_choice = rand() % 3; 
+	unsigned char pc_choice = rand() % 3; 
 
-	if (!strcmp(game_elements[pc_choice], user_choice)) {
-		END_GAME_MESSAGE(user_choice, game_elements[pc_choice], "DRAW!");
-	} else if ((!strcmp(game_elements[pc_choice], "r") && !strcmp(user_choice, "p")) ||
-				(!strcmp(game_elements[pc_choice], "p") && !strcmp(user_choice, "s")) ||
-				(!strcmp(game_elements[pc_choice], "s") && !strcmp(user_choice, "r"))) {
-		END_GAME_MESSAGE(user_choice, game_elements[pc_choice], "YOU WINS!");
+	if (user_choice == game_elements[pc_choice]) {
+		end_game_message(user_choice, game_elements[pc_choice], "DRAW!");
+	} else if ((game_elements[pc_choice] == 'r' && user_choice == 'p') ||
+				(game_elements[pc_choice] == 'p' &&  user_choice == 's') ||
+				(game_elements[pc_choice] == 's' &&  user_choice == 'r')) {
+		end_game_message(user_choice, game_elements[pc_choice], "YOU WINS!");
 	} else {
-		END_GAME_MESSAGE(user_choice, game_elements[pc_choice], "PC WINS!");
+		end_game_message(user_choice, game_elements[pc_choice], "PC WINS!");
 	}
 
 end:
