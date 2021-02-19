d0a13326ba79e0f564c7c4bb15d6fc569a604e9b
Nazar Sopiha <nazar.sopiha@gmail.com>
2021-02-19 02:08:25 +0200

    Detect i2c and flash
    It is now detecting i2c and flash connected devices
    Then script goes into an infinite cycle -
    for further decetions of hot plugged and unplugged hardware
    
    Signed-off-by: Nazar Sopiha <nazar.sopiha@gmail.com>

--- hwdetect/hwdetect.sh
+++ hwdetect/hwdetect.sh
@@ -1,68 +1,57 @@
 #!/bin/bash
 
 #TODO: Detect plugged and unplugged irt
-#	
 
 
 
 
-#somewhy this function gets an error ./hwdetect.sh: line 15: i2c_devices: command not found
-
-#function plugged_i2c()
-#{
-#	i2c_devices = $(ls /dev/i2c-*)
-#	echo "****Connected I2C****"
-#	for i2c in $i2c_devices
-#	do
-#		echo $i2c
-#	done
-#}
+function plugged_i2c()
+{
+	i2c_devices=$(ls /dev/i2c-*)
+	if [ -n "$i2c_devices" ] ; then
+		echo "****Connected I2C****"
+		for i2c in $i2c_devices ; do
+			echo $i2c
+		done
+	fi
+}
 
 #credits to olegovich22, like his implementation
 function plugged_flash_drivers()
 {
 	flash_devices=$(ls /dev/sd*)
-	if [ -n "$flash_devices" ]
-	then
-
-	echo
-	echo "****Connected Flash drivers****"
-
-	for d in $flash_devices 
-	do
-		tput setaf 1
-	 	echo -n "Detected flash driver controler on "
-		echo $d
-		tput sgr0
-		tput setaf 2
-		echo $(udevadm info $d | grep -oE "MODEL=.*")
-		echo $(udevadm info $d | grep -oE "VENDOR=.*")
-		echo -n $(udevadm info $d | grep -oE "BUS=.*")
-		tput sgr0
+	if [ -n "$flash_devices" ] ; then 
 		echo
-	done
+		echo "****Connected Flash drivers****"
 
-	echo
-
+		for d in $flash_devices ; do
+			tput setaf 1
+		 	echo -n "Detected flash driver controler on "
+			echo $d
+			tput sgr0
+			tput setaf 2
+			echo $(udevadm info $d | grep -oE "MODEL=.*")
+			echo $(udevadm info $d | grep -oE "VENDOR=.*")
+			echo -n $(udevadm info $d | grep -oE "BUS=.*")
+			tput sgr0
+			echo
+		done
 	fi
 }
 
 
-echo "Checking connected HW"
+echo "****Checking connected HW****"
+echo " "
 
 
-echo "****Connected I2C****"
-for i2c in $(ls /dev/i2c-*)
-do
-	echo $i2c
-done
-
+plugged_i2c
 plugged_flash_drivers
+
 
 while true
 do
 	echo "Detecting plugged and unplugged hardware. TBD"
 	#check in real time
-sleep 1
+sleep 3
 done
 
