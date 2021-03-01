7ea9693e8312bc75aec39ae137711b34da215564
ASynenko <andriysinenko@gmail.com>
2021-03-01 12:46:07 +0200

    04_basic_struct: Fix changes requested by review
    Add bonus task implementation
    
    Signed-off-by: ASynenko <andriysinenko@gmail.com>

--- 04_basic_struct/lds_module.c
+++ 04_basic_struct/lds_module.c
@@ -12,118 +12,143 @@
 #include <linux/list.h>
 #include <linux/slab.h>
 
+//#define DEBUG
+
 MODULE_AUTHOR("Andrii Synenko");
 MODULE_DESCRIPTION("List data module for Linux Kernel ProCamp");
 MODULE_LICENSE("Dual BSD/GPL");
 
-struct list_data {
-	struct list_head my_list;
-	int data;
+struct data_node {
+	struct list_head data_list;
+	size_t data_size;
+	const char *data_ptr;
 };
 
-static LIST_HEAD(data_list_head);
+struct accum_data {
+	struct list_head accum_list;
+	size_t accum_size;
+} gl_accum;
 
-static int list_entry_add(int data, struct list_head *head)
+static int accum_entry_add(struct accum_data *accum, const char *new_data_prt,
+						  size_t new_data_size)
 {
-	struct list_data *entry;
+	struct data_node *entry;
 
-	if (head == NULL) {
-		pr_err("Linked list doesn't exist!\n");
+	if ((accum == NULL) || (new_data_prt == NULL)) {
+		pr_err("Linked list of input doesn't exist!\n");
 		return -EINVAL;
 	}
 
-	entry = kmalloc(sizeof(struct list_data), GFP_KERNEL);
-
+	entry = kmalloc(sizeof(*entry), GFP_KERNEL);
 	if (entry == NULL)
 		return -ENOMEM;
 
-	entry->data = data;
+	entry->data_ptr = kstrdup(new_data_prt, GFP_KERNEL);
 
-	list_add(&entry->my_list, head);
+	if (entry->data_ptr == NULL)
+		return -ENOMEM;
+
+	entry->data_size = new_data_size;
+	accum->accum_size += entry->data_size;
+	list_add_tail(&entry->data_list, &accum->accum_list);
 
 	return 0;
 }
 
-static int list_print(struct list_head *head, char *buf)
+static int accum_free_from_head(struct accum_data *aaccum, size_t req_space)
+{
+	struct data_node *entry = NULL;
+
+	if (aaccum == NULL) {
+		pr_err("Linked list doesn't exist!\n");
+		return -EINVAL;
+	}
+
+	while ((aaccum->accum_size + req_space) > PAGE_SIZE) {
+
+		if (list_empty(&aaccum->accum_list) == 1)
+			return -EINVAL;
+
+		entry = list_first_entry(&aaccum->accum_list, struct data_node,
+								 data_list);
+
+		list_del(&entry->data_list);
+		kfree(entry->data_ptr);
+		aaccum->accum_size -= entry->data_size;
+		kfree(entry);
+	}
+
+	return 0;
+}
+
+static int accum_print(struct accum_data *aaccum, char *buf)
 {
 	size_t list_size = 0;
 	int res = 0;
 	int cursor = 0;
 
-	struct list_data *entry = NULL;
+	struct data_node *entry = NULL;
 
-	if ((head == NULL) || (buf == NULL)) {
+	if ((aaccum == NULL) || (buf == NULL)) {
 		pr_err("Linked list or output buffer doesn't exist!\n");
 		return -EINVAL;
 	}
 
-	res += sprintf(&buf[cursor], "[head] => ");
-	if (res > 0)
-		cursor += res;
-	else {
-		pr_err("Failed to write output.\n");
-		return -EIO;
-	}
-
-	list_for_each_entry(entry, head, my_list) {
-		res = sprintf(&buf[cursor], "[%d] => ", entry->data);
+	list_for_each_entry(entry, &aaccum->accum_list, data_list) {
+		res = sprintf(&buf[cursor], "%s\n", entry->data_ptr);
 		if (res > 0)
 			cursor += res;
 		else {
 			pr_err("Failed to write output.\n");
 			return -EIO;
 		}
+
 		list_size++;
 	}
 
-	res = sprintf(&buf[cursor], "[head], size = %d node(s)\n",
-		list_size);
-
-	if (res > 0)
-		cursor += res;
-	else {
-		pr_err("Failed to write output.\n");
-		return -EIO;
-	}
-
-	return 0;
+	return cursor;
 }
 
-static int list_free(struct list_head *head)
+static int accum_free(struct accum_data *aaccum)
 {
-	struct list_data *entry = NULL;
-	struct list_head *position = NULL,
+	struct data_node *entry = NULL,
 					 *tmp = NULL;
 
-	if (head == NULL) {
+	if (aaccum == NULL) {
 		pr_err("Linked list doesn't exist!\n");
 		return -EINVAL;
 	}
 
-	list_for_each_safe(position, tmp, head) {
-		entry = list_entry(position, struct list_data, my_list);
-		list_del(position);
+	list_for_each_entry_safe(entry, tmp, &aaccum->accum_list, data_list) {
+
+		list_del(&entry->data_list);
+		kfree(entry->data_ptr);
+		aaccum->accum_size -= entry->data_size;
 		kfree(entry);
 	}
 	return 0;
 }
 
-static ssize_t list_show(struct kobject *kobj, struct kobj_attribute *attr,
+static ssize_t accum_show(struct kobject *kobj, struct kobj_attribute *attr,
 					   char *buf)
 {
-	list_print(&data_list_head, buf);
-	return strlen(buf);
+	return accum_print(&gl_accum, buf);
 }
 
-static ssize_t list_store(struct kobject *kobj, struct kobj_attribute *attr,
+static ssize_t accum_store(struct kobject *kobj, struct kobj_attribute *attr,
 						const char *buf, size_t count)
 {
 	int res = 0;
-	int tmp_value = 0;
 
-	res = sscanf(buf, "%du", &tmp_value);
-	if (res > 0)
-		list_entry_add(tmp_value, &data_list_head);
+	res = accum_free_from_head(&gl_accum, count);
+
+	if (res != 0)
+		return res;
+
+	res = accum_entry_add(&gl_accum, buf, count);
+
+	if (res != 0)
+		return res;
 
 	return count;
 }
@@ -131,8 +156,8 @@ static ssize_t list_store(struct kobject *kobj, struct kobj_attribute *attr,
 static struct kobject *hello_kobject;
 struct kobj_attribute kobj_attr_list = {
 	.attr = { .name = "list", .mode = 0666 },
-	.show	= list_show,
-	.store	= list_store
+	.show	= accum_show,
+	.store	= accum_store
 };
 
 static int init_sysfs(void)
@@ -159,6 +184,14 @@ static int __init lds_module_init(void)
 {
 	int res = 0;
 
+	INIT_LIST_HEAD(&gl_accum.accum_list);
+
+#ifdef DEBUG
+	gl_accum.accum_size = PAGE_SIZE - 10;
+#else
+	gl_accum.accum_size = 0;
+#endif
+
 	pr_info("lds_module: module starting\n");
 
 	res = init_sysfs();
@@ -175,7 +208,7 @@ static void __exit lds_module_exit(void)
 		kobject_del(hello_kobject);
 	}
 
-	list_free(&data_list_head);
+	accum_free(&gl_accum);
 	pr_info("lds_module: module exited\n");
 }
 
