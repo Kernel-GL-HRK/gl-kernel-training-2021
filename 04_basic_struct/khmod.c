870d6a96122453405d0b67b102ad9792b667179e
Vadym Kharchuk <vadym.kharchuk@globallogic.com>
2021-03-01 18:47:51 +0200

    04_basic_struct: Add linked list, and basic sysfs show list
    Signed-off-by: Vadym Kharchuk <vadym.kharchuk@globallogic.com>

--- 04_basic_struct/khmod.c
+++ 04_basic_struct/khmod.c
@@ -3,46 +3,67 @@
 #include <linux/module.h>
 #include <linux/device.h>
 #include <linux/err.h>
+#include <linux/list.h>
 
 MODULE_DESCRIPTION("Sysfs llist demo module");
 MODULE_AUTHOR("vadym.kharchuk@globallogic.com");
 MODULE_VERSION("0.1");
 MODULE_LICENSE("Dual MIT/GPL");
 
-static char user[PAGE_SIZE] = "test";
+struct list_node {
+	struct list_head list;
+	char *data;
+};
+
+static struct class *attr_class = 0;
+
+struct list_node first = {
+	.data = "data1",
+	.list = LIST_HEAD_INIT(first.list)
+};
+
+struct list_node second = {
+	.data = "data2",
+	.list = LIST_HEAD_INIT(second.list)
+};
+
+LIST_HEAD(mylist);
 
 static ssize_t rw_show(struct class *class, struct class_attribute *attr, char *buf)
 {
-	pr_info("%s: rw_show: name = %s\n", THIS_MODULE->name, user);
+	struct list_node *list_node_ptr = NULL;
+	
+	list_for_each_entry_reverse(list_node_ptr, &mylist, list) {
+		strcat(buf, list_node_ptr->data);
+		strcat(buf, " ");
+	}
+	strcat(buf, "\n");
 
-	sprintf(buf, "%s\n", user);
 	return strlen(buf);
 }
 
 static ssize_t rw_store(struct class *class, struct class_attribute *attr, const char *buf, size_t count)
 {
-	sscanf(buf, "%s", user);
-
-	pr_info("%s: rw_store: value = %s\n", THIS_MODULE->name, user);
 	return count;
 }
+
 struct class_attribute class_attr_rw = {
 	.attr = { .name = "rw", .mode = 0666 },
 	.show	= rw_show,
 	.store	= rw_store
 };
 
-static struct class *attr_class = 0;
-
 static int __init firstmod_init(void)
 {
 	int ret = 0;
 	attr_class = class_create(THIS_MODULE, "kharchuk");
+	
 	if (attr_class == NULL) {
 		pr_err("%s: error creating sysfs class\n", THIS_MODULE->name);
 		return -ENOMEM;
 	}
 	ret = class_create_file(attr_class, &class_attr_rw);
+	
 	if (ret) {
 		pr_err("%s: error creating sysfs class attribute\n", THIS_MODULE->name);
 		class_destroy(attr_class);
@@ -50,6 +71,10 @@ static int __init firstmod_init(void)
 	}
 
 	pr_info("%s: loaded!\n", THIS_MODULE->name);
+	
+	list_add(&first.list, &mylist);
+	list_add(&second.list, &mylist);
+	
 	return 0;
 }
 
@@ -57,7 +82,8 @@ static void __exit firstmod_exit(void)
 {
 	class_remove_file(attr_class, &class_attr_rw);
 	class_destroy(attr_class);
-	pr_info("See u, %s", user);
+	
+	pr_info("%s: removed!\n", THIS_MODULE->name);
 }
 
 module_init(firstmod_init);
