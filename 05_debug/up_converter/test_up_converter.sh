0fc6c2feb6b59759b42d03163d336bdd2801a85e
olyuboch <lyubochko.leha@gmail.com>
2021-03-03 12:45:04 +0200

    05_debug: change testing script for testing cycling buffer
    Signed-off-by: olyuboch <lyubochko.leha@gmail.com>

--- 05_debug/up_converter/test_up_converter.sh
+++ 05_debug/up_converter/test_up_converter.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-test_string_lower="abcdefghigklmnopqrstuvwxyz0123456789></.,][{}"
-test_string_upper="ABCDEFGHIGKLMNOPQRSTUVWXYZ0123456789></.,][{}"
+test_string_lower="abcdefghigklmnopqrs"
+test_string_upper="ABCDEFGHIGKLMNOPQRS"
+test_string_large="abcdefghigklmnopqrstuvwxyz0123456789></.,][{}"
 module="up_converter.ko"
 
 echo "Testing up_converter moudule"
@@ -10,18 +11,54 @@ echo "Print statistic"
 cat /proc/up_conv/stat
 echo "Read datat from converter"
 cat /proc/up_conv/converter
+
+echo ""
+echo "Test too large string"
+echo "Write data to converter:" ${test_string_large}
+sleep 0.1s
+echo ${test_string_large} > /proc/up_conv/converter
+echo "Read data from converter"
+cat /proc/up_conv/converter
+echo ""
+echo "Print statistic"
+cat /proc/up_conv/stat
+
+echo ""
+echo "Test for cycling buffer"
 echo "Write data to converter:" ${test_string_lower}
 echo ${test_string_lower} > /proc/up_conv/converter
 echo "Read data from converter"
 cat /proc/up_conv/converter
 echo "Print statistic"
 cat /proc/up_conv/stat
+
+echo ""
+echo "Test for cycling buffer"
 echo "Write data to converter:" ${test_string_upper}
 echo ${test_string_upper} > /proc/up_conv/converter
 echo "Read data from converter"
 cat /proc/up_conv/converter
 echo "Print statistic"
 cat /proc/up_conv/stat
+
+echo ""
+echo "Test for cycling buffer"
+echo "Write data to converter:" ${test_string_lower}
+echo ${test_string_lower} > /proc/up_conv/converter
+echo "Read data from converter"
+cat /proc/up_conv/converter
+echo "Print statistic"
+cat /proc/up_conv/stat
+
+echo ""
+echo "Test for cycling buffer"
+echo "Write data to converter:" ${test_string_lower}
+echo ${test_string_lower} > /proc/up_conv/converter
+echo "Read data from converter"
+cat /proc/up_conv/converter
+echo "Print statistic"
+cat /proc/up_conv/stat
+
 sleep 0.2s
 echo ${module}
 rmmod ${module}
