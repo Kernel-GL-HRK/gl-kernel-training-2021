c937bd0d357fd2f6bf362c47af9b6a9d423bae09
olyuboch <lyubochko.leha@gmail.com>
2021-03-04 19:02:27 +0200

    06_timers: add high precision time resolution for user space app
    Signed-off-by: olyuboch <lyubochko.leha@gmail.com>

--- 06_timers/01_absolute_time/main.c
+++ 06_timers/01_absolute_time/main.c
@@ -10,24 +10,27 @@ const char *months[] = {"January", "February", "March", "April", "May",
 	"December" };
 
 int main(void) {
+	int ret;
 	struct tm *ptm;
-	time_t rawtime = time(NULL);
+	struct timespec spec;
 
-	if (rawtime == -1) {
-		perror("The time() function failed");
+	ret = clock_gettime(CLOCK_REALTIME, &spec);
+
+	if (ret == -1) {
+		perror("The clock_gettime() function failed");
 		return -1;
 	}
 
-	ptm = localtime(&rawtime);
+	ptm = localtime(&spec.tv_sec);
 
 	if (ptm == NULL) {
 		perror("The localtime() function failed");
 		return -1;
 	}
 
-	printf("%s, %02d %s %d %02d:%02d:%02d\n", day_of_week[ptm->tm_wday],
+	printf("%s, %02d %s %d %02d:%02d:%02d.%09ld\n", day_of_week[ptm->tm_wday],
 		ptm->tm_mday, months[ptm->tm_mon], YEARS_SINCE + ptm->tm_year,
-		ptm->tm_hour, ptm->tm_min, ptm->tm_sec);
+		ptm->tm_hour, ptm->tm_min, ptm->tm_sec, spec.tv_nsec);
 
 	return 0;
 }
