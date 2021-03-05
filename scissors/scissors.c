5456c247713f3a6dee37cc6c3e4ee93a63382e97
Vladyslav Andrishko <v.andrishko.v333@gmail.com>
2021-03-05 14:41:21 +0200

    Scissors_game: change data type from 'char*' to 'char'
    Changes data types for optimizing app
    
    Signed-off-by: Vladyslav Andrishko <v.andrishko.v333@gmail.com>

--- scissors/scissors.c
+++ scissors/scissors.c
@@ -3,55 +3,50 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *define_computer(int num, char *comp_var)
+static char define_computer(int num)
 {
+	char comp_var;
 
 	if (num == 0)
-		strcpy(comp_var, "p\0");
+		comp_var = 'p';
 	else if (num == 1)
-		strcpy(comp_var, "s\0");
+		comp_var = 's';
 	else if (num == 2)
-		strcpy(comp_var, "r\0");
+		comp_var = 'r';
 	return (comp_var);
 
 }
 
-void print_result(char *human_var, char *computer_var, int win)
+static void print_result(char human_var, char computer_var, int win)
 {
 	if (win == 1)
 		printf("\nYeap. You win.\n");
 	else
 		printf("\nOps. You lose.\n");
-	printf("Because you pick %s and  computer pick %s\n", human_var, computer_var);
+	printf("Because you pick %c and  computer pick %c\n", human_var, computer_var);
 		
 }
 
-void game_start(char *human_var)
+static void game_start(char human_var)
 {
 	srand(time(NULL));
 	int r = rand() % 2;
-	char computer_var[2];
-	strcpy(computer_var,define_computer(r, computer_var));
+	char computer_var;
+	computer_var = define_computer(r);
 
-	if (strcmp(computer_var, human_var) == 0)
-		printf("\nHmm...Nice try, but it`s draw.You both pick %s\n", human_var);
-	else if (strcmp(computer_var, "p") == 0 && \
-		strcmp(human_var, "s") == 0)
+	if (computer_var == human_var)
+		printf("\nHmm...Nice try, but it`s draw.You both pick %c\n", human_var);
+	else if (computer_var == 'p' && human_var == 's')
 		print_result(human_var, computer_var, 1);
-	else if (strcmp(computer_var, "r") == 0 && \
-		strcmp(human_var, "p") == 0)
+	else if (computer_var == 'r' && human_var == 'p')
 		print_result(human_var, computer_var, 1);
-	else if (strcmp(computer_var, "s") == 0 && \
-		strcmp(human_var, "r") == 0)
+	else if (computer_var == 's' && human_var == 'r')
 		print_result(human_var, computer_var, 1);
-	else if (strcmp(computer_var, "s") == 0 && \
-		strcmp(human_var, "p") == 0)
+	else if (computer_var == 's' && human_var == 'p')
 		print_result(human_var, computer_var, 0);
-	else if (strcmp(computer_var, "p") == 0 && \
-		strcmp(human_var, "r") == 0)
+	else if (computer_var == 'p' && human_var == 'r')
 		print_result(human_var, computer_var, 0);
-	else if (strcmp(computer_var, "r") == 0 && \
-		strcmp(human_var, "s") == 0)
+	else if (computer_var == 'r' && human_var == 's')
 		print_result(human_var, computer_var, 0);
 	else 
 		printf("\nHmm. I dont know this symbol.Please try again.\n");
@@ -63,7 +58,7 @@ int main(int argc, char **argv)
 	if (argc == 1)
 	        puts("Please choose: rock (r) - paper (p) - scissors (s)");
 	else if (argc == 2)
-		game_start(argv[1]);
+		game_start(argv[1][0]);
 	return (0);
 }
 
