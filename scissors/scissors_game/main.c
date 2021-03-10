17d01f6648fdadad5e78836a6f2ee89e5e054e95
Maryna Malakhova <maryna.malakhova@globallogic.com>
2021-03-10 15:47:53 +0200

    SCISSORS: Add reading users input and it`s convertion to text
    Add users input reading and printing it out after convertion to text
    
    Signed-off-by: Maryna Malakhova <maryna.malakhova@globallogic.com>

--- scissors/scissors_game/main.c
+++ scissors/scissors_game/main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+
+void static char_to_text(char c, char *res, int size)
+{
+	switch(c) {
+	case 'r':
+		strncpy(res, "rock", size);
+		break;
+	case 'p':
+		strncpy(res, "paper", size);
+		break;
+	case 's':
+		strncpy(res, "scissors", size);
+		break;
+	default:
+		strncpy(res, "unknown", size);
+	}
+}
 
 int main(void)
 {
-	printf("Please choose: rock (r) - paper (p) - scissors (s)\n");
+	while(1){
+		printf("Please choose: rock (r) - paper (p) - scissors (s)\n");
+		char input;
+		char res [10];
+		memset (res, 0, 10);
+		scanf ("%1s", &input);
+		char_to_text(input, res, sizeof(res)-1);
+		printf("You choose %s and it's ok!\n", res);
+	}
 }
