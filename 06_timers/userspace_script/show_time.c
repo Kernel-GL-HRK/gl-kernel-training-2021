71413fe1bc1ceb104ab7cad861f66d7c08b72e48
Denys Andreichuk <denanko@gmail.com>
2021-03-10 08:58:58 +0200

    Task_06: Timers
    Requested change to userspace program
    
    Signed-off-by: Denys Andreichuk <denanko@gmail.com>

--- 06_timers/userspace_script/show_time.c
+++ 06_timers/userspace_script/show_time.c
@@ -4,10 +4,15 @@
 int main(void)
 {
 	struct timespec now = {0};
+	struct tm *hrt = {0};		// human readable time
 
 	clock_gettime(0, &now);
+	hrt = localtime(&now.tv_sec);
 
-	printf("Absolute time is %ld.%ld seconds.\n", now.tv_sec, now.tv_nsec);
+	printf("It is %d:%d:%d %d-%d-%d\n",
+			hrt->tm_hour, hrt->tm_min, hrt->tm_sec,
+			hrt->tm_year + 1900,
+			hrt->tm_mon, hrt->tm_mday);
 
 	return 0;
 }
