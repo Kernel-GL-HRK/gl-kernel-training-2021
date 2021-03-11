214ccac8bbb056687e6529de37178655335b5299
Maryna Malakhova <maryna.malakhova@globallogic.com>
2021-03-11 20:06:12 +0200

    SCISSORS: Move enums to <scissors.h>
    Create header and move enums there
    
    Signed-off-by: Maryna Malakhova <maryna.malakhova@globallogic.com>

--- scissors/scissors_game/main.c
+++ scissors/scissors_game/main.c
@@ -2,27 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
-
-typedef enum {
-	eDRAW = -1,
-	eLOST,
-	eWINN
-} decision_t;
-
-/*Enum of Unknown, Rock, Paper and Scissors choise*/
-typedef enum {
-	eU = -1,
-	eR,
-	eP,
-	eS
-
-} element_t;
-
-const int game_table[3][3]={
-	eDRAW,   eLOST,  eWINN,
-	eWINN,   eDRAW,  eLOST,
-	eLOST,   eWINN,  eDRAW
-};
+#include "scissors.h"
 
 int static char_to_text(char c, char *res, int size)
 {
