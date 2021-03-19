fa247a4e1dc740a465ddc93c907bd2b2ee042a00
Maryna Malakhova <maryna.malakhova@globallogic.com>
2021-03-19 14:47:20 +0200

    BSTRUCTMODULE: Add list
    Add list of strings insted of int value
    
    Signed-off-by: Maryna Malakhova <maryna.malakhova@globallogic.com>

--- 04_basic_struct/hello.c
+++ 04_basic_struct/hello.c
@@ -7,22 +7,44 @@
 #include <linux/err.h>
 #include <linux/string.h>
 #include <linux/sysfs.h>
+#include <linux/list.h>
+#include <linux/slab.h>
 
-static int value;
+struct list_node {
+	char *node;
+	struct list_head head_list;
+};
+
+static LIST_HEAD(linkedlist);
 
 static ssize_t hello_show(struct kobject *kobj, struct kobj_attribute *attr,
 	char *buf)
 {
-	pr_info("%s: value = %d\n", __func__, value);
-	sprintf(buf, "%d\n", value);
-	return strlen(buf);
+	struct list_node *cur_node = NULL;
+	int offset = 0;
+	int res = 0;
+	int cnt = 1;
+
+	list_for_each_entry(cur_node, &linkedlist, head_list) {
+		res = scnprintf(buf+offset, PAGE_SIZE-offset, "Node %d: %s",
+			cnt++, cur_node->node);
+		offset += res;
+	}
+	return offset;
 }
 
 static ssize_t hello_store(struct kobject *kobj, struct kobj_attribute *attr,
 	const char *buf, size_t count)
 {
-	if (kstrtoint (buf, 0, &value))
-		pr_err("Invalid value\n");
+	struct list_node *new_node =
+		kmalloc(sizeof(struct list_node), GFP_KERNEL);
+
+	if (!new_node)
+		return -ENOMEM;
+	new_node->node = kstrdup(buf, GFP_KERNEL);
+	if (new_node->node == NULL)
+		return -ENOMEM;
+	list_add_tail(&new_node->head_list, &linkedlist);
 	return count;
 }
 
@@ -46,6 +68,15 @@ static int __init hello_init(void)
 
 static void __exit hello_exit(void)
 {
+	struct list_node *cur_node = NULL;
+	struct list_node *tmp_node = NULL;
+
+	list_for_each_entry_safe(cur_node, tmp_node, &linkedlist, head_list) {
+		pr_info("remove node: %s", cur_node->node);
+		list_del(&cur_node->head_list);
+		kfree(cur_node->node);
+		kfree(cur_node);
+	}
 	kobject_put(hello_kobj);
 	pr_info("module exited\n");
 }
