91838223a5751e01c02c8a9e0ad515d90830cbbb
Anton Kotsiubailo <antohakotsubailo@gmail.com>
2021-03-22 20:07:02 +0200

    task03: Repair firstmod
    Signed-off-by: Anton Kotsiubailo <antohakotsubailo@gmail.com>

--- 03_module/firstmod.c
+++ 03_module/firstmod.c
@@ -1,4 +1,5 @@
-#include <linux/init.h>    // used by module_init, module_exit macros
+// SPDX-License-Identifier: MIT and GPL
+#include <linux/init.h>
 #include <linux/jiffies.h> // where jiffies and its helpers reside
 #include <linux/kernel.h>  // required for sysinfo
 #include <linux/module.h>  // required by all modules
@@ -7,7 +8,7 @@
 MODULE_DESCRIPTION("Basic module demo: init, deinit, printk, jiffies");
 MODULE_AUTHOR("Anton Kotsiubailo");
 MODULE_VERSION("0.1");
-MODULE_LICENSE("Dual MIT/GPL"); // this affects the kernel behavior
+MODULE_LICENSE("GPL"); // this affects the kernel behavior
 
 static long count_start;
 static char *usrname;
@@ -17,19 +18,22 @@ MODULE_PARM_DESC(usrname, "user name");
 
 static int __init firstmod_init(void)
 {
-  count_start = jiffies;
-  char str[] = "$usrname";
-  if (!usrname) {
-    printk(KERN_WARNING "username wasn`t passed as a parameter\n");
-    usrname = str;
-  }
-
-  printk(KERN_INFO "Hello, %s!\n", usrname);
-
-  return 0;
+count_start = jiffies;
+char str[] = "$usrname";
+if (!usrname) {
+	pr_warn("username wasn`t passed as a parameter\n");
+	usrname = str;
 }
 
-static void __exit firstmod_exit(void) { printk(KERN_INFO "Goodbye\n"); }
+pr_warn("Hello, %s!\n", usrname);
+
+return 0;
+}
+
+static void __exit firstmod_exit(void)
+{
+pr_info("Goodbye\n");
+}
 
 module_init(firstmod_init);
 module_exit(firstmod_exit);
