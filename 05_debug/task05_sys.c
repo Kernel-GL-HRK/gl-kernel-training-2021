c680cf329ea1131ff951f9088829e4edbd5c6d55
Anton Kotsiubailo <antohakotsubailo@gmail.com>
2021-03-25 13:55:49 +0200

    kernel: Modify proc and sysfs code
    Performed refactoring of existed code
    
    Signed-off-by: Anton Kotsiubailo <antohakotsubailo@gmail.com>

--- 05_debug/task05_sys.c
+++ 05_debug/task05_sys.c
@@ -14,6 +14,7 @@ MODULE_VERSION("0.1");
 MODULE_LICENSE("Dual MIT/GPL"); // this affects the kernel behavior
 
 #define LEN_MSG 160
+
 #define CLASS_ATTR(_name, _mode, _show, _store)	\
 struct class_attribute class_attr_##_name =	\
 __ATTR(_name, _mode, _show, _store)
@@ -26,33 +27,40 @@ static uint32_t total_calls;
 static ssize_t x_show(struct class *class, struct class_attribute *attr,
 			char *buf)
 {
-strcpy(buf, buf_msg);
-pr_info("read %ld\n", (long)strlen(buf));
-total_calls++;
-return strlen(buf);
+	strcpy(buf, buf_msg);
+
+	pr_info("read %ld\n", (long)strlen(buf));
+
+	total_calls++;
+
+	return strlen(buf);
 }
 
 static ssize_t x_store(struct class *class, struct class_attribute *attr,
 			const char *buf, size_t count)
 {
-int i;
-if (count > LEN_MSG) {
-	pr_err("Entered string is too long\n");
-	return -EINVAL;
-}
-strncpy(buf_msg, buf, count);
+	int i;
 
-for (i = 0; buf_msg[i] != '\0'; i++) {
-	if (buf_msg[i] >= 'A' && buf_msg[i] <= 'Z') {
-		buf_msg[i] = buf_msg[i] + 32;
-		characters_converted++;
+	if (count > LEN_MSG) {
+		pr_err("Entered string is too long\n");
+		return -EINVAL;
 	}
-}
-total_characters_processed = count;
-pr_info("write %ld\n", (long)count);
-buf_msg[count] = '\0';
-return count;
 
+	strncpy(buf_msg, buf, count);
+	for (i = 0; buf_msg[i] != '\0'; i++) {
+		if (buf_msg[i] >= 'A' && buf_msg[i] <= 'Z') {
+			buf_msg[i] = buf_msg[i] + 32;
+			characters_converted++;
+		}
+	}
+
+	total_characters_processed = count;
+
+	pr_info("write %ld\n", (long)count);
+
+	buf_msg[count] = '\0';
+
+	return count;
 }
 
 static ssize_t total_characters_processed_show(struct class *class,
@@ -77,58 +85,64 @@ static ssize_t total_calls_show(struct class *class,
 }
 
 CLASS_ATTR(xxx, 0644, &x_show, &x_store);
+
 CLASS_ATTR(total_characters_processed, 0444,
 		&total_characters_processed_show, NULL);
+
 CLASS_ATTR(total_calls, 0444, &total_calls_show, NULL);
+
 CLASS_ATTR(characters_converted, 0444, &characters_converted_show, NULL);
 
 static struct class *x_class;
 
 int __init x_init(void)
 {
-int res;
-x_class = class_create(THIS_MODULE, "x-class");
-if (IS_ERR(x_class)) {
-	pr_err("bad class create\n");
-	return -EINVAL;
-}
-res = class_create_file(x_class, &class_attr_xxx);
-if (res != 0) {
-	pr_err("bad xxx class file create\n");
-	class_destroy(x_class);
-	return -EINVAL;
+	int res;
+
+	x_class = class_create(THIS_MODULE, "x-class");
+
+	if (IS_ERR(x_class)) {
+		pr_err("bad class create\n");
+		return -EINVAL;
+	}
+
+	res = class_create_file(x_class, &class_attr_xxx);
+	if (res != 0) {
+		pr_err("bad xxx class file create\n");
+		class_destroy(x_class);
+		return -EINVAL;
+	}
+
+	res = class_create_file(x_class,
+				&class_attr_total_characters_processed);
+	if (res != 0) {
+		pr_err("bad file 'total_characters_processed' create\n");
+		return -EINVAL;
+	}
+
+	res = class_create_file(x_class, &class_attr_total_calls);
+	if (res != 0) {
+		pr_err("bad file 'total_calls' create\n");
+		return -EINVAL;
+	}
+
+	res = class_create_file(x_class, &class_attr_characters_converted);
+	if (res != 0) {
+		pr_err("bad file 'characters_converted' create\n");
+		return -EINVAL;
+	}
+
+	pr_info("'xxx' module initialized\n");
+	return res;
 }
 
-res = class_create_file(x_class, &class_attr_total_characters_processed);
-if (res != 0) {
-	pr_err("bad file 'total_characters_processed' create\n");
-	return -EINVAL;
-}
-
-res = class_create_file(x_class, &class_attr_total_calls);
-if (res != 0) {
-	pr_err("bad file 'total_calls' create\n");
-	return -EINVAL;
-}
-
-res = class_create_file(x_class, &class_attr_characters_converted);
-if (res != 0) {
-	pr_err("bad file 'characters_converted' create\n");
-	return -EINVAL;
-}
-pr_info("'xxx' module initialized\n");
-return res;
-}
-
-void x_cleanup(void)
+void __exit x_cleanup(void)
 {
-class_remove_file(x_class, &class_attr_characters_converted);
-class_remove_file(x_class, &class_attr_total_calls);
-class_remove_file(x_class, &class_attr_total_characters_processed);
-class_remove_file(x_class, &class_attr_xxx);
-class_destroy(x_class);
-
-return;
+	class_remove_file(x_class, &class_attr_characters_converted);
+	class_remove_file(x_class, &class_attr_total_calls);
+	class_remove_file(x_class, &class_attr_total_characters_processed);
+	class_remove_file(x_class, &class_attr_xxx);
+	class_destroy(x_class);
 }
 
 module_init(x_init);
