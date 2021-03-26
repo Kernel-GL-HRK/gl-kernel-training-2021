5d03715bdeff3f1dc0706e72fd0a23542d49b0ac
Vadym Kharchuk <vadimkharchuk@gmail.com>
2021-03-26 22:20:59 +0200

    task_11: Add ds3231x driver implementation
    Signed-off-by: Vadym Kharchuk <vadimkharchuk@gmail.com>

---
+++ 11_rtc_driver/ds3231.h
@@ -0,0 +1,14 @@
+#ifndef _DS3231_H_
+#define _DS3231_H_
+
+#define REG_NUM	7
+
+#define DS3231_SEC_REG		0x00
+#define DS3231_MIN_REG		0x01
+#define DS3231_HOUR_REG		0x02
+#define DS3231_DAY_REG		0x03
+#define DS3231_DATE_REG		0x04
+#define DS3231_MONTH_REG	0x05
+#define DS3231_YEAR_REG		0x06
+
+#endif
